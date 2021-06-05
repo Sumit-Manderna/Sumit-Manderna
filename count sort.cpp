fe7e48a1fa1557e5b129131e53a37094f8aadf19
Sumit Manderna <77228139+Sumit-Manderna@users.noreply.github.com>
2021-06-05 22:50:44 +0530

    Add files via upload

---
+++ count sort.cpp
@@ -0,0 +1,41 @@
+// sort the given arrray using count sort method 
+#include<iostream>
+using namespace std;
+
+void countSort(int arr[],int n)
+{
+    int k=arr[0];
+    for(int i=0;i<n;i++){
+        k=max(k,arr[i]);
+    }
+    int count[10]={0};
+    
+    for(int i=0;i<n;i++)
+    {
+        count[arr[i]]++;
+    }
+
+    for(int i=1;i<=k;i++)
+    {
+        count[i]+=count[i-1]; 
+    }
+    
+    int output[n];
+    for(int i=n-1;i>=0;i--)
+    {
+        int temp=arr[i];
+        output[--count[temp]]=temp;
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=output[i];
+    }
+
+}
+int main(){
+    int arr[]={1,3,2,3,4,1,6,4,3};
+    countSort(arr,9);
+    for(int i=0;i<9;i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
